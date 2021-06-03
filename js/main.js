47538fa7474a838975f583f9905a8a416fbe9af5
Aleksey978 <70477391+Aleksey978@users.noreply.github.com>
2021-06-03 20:16:06 +0900

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 let randomNumber = (min, max) => {
+  if(min >= 0 && max >= min) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return 'ощибка';
 }
 
 randomNumber(1, 10);
 
 let allowedLength = 140;
 
-let checksСommentLength = (length, allowedLength) => {
-  if (length <= allowedLength) {
+let checksСommentLength = (comment, allowedLength) => {
+  if (comment.length <= allowedLength) {
     return true;
   }
 }
